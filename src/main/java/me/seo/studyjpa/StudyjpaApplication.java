d43ad081ed7bc1d42d12d6397a1e82c7ec1dcca6
tjwjdgks <tjwjdgks43@gmail.com>
2021-08-19 16:24:44 +0900

    jpa stduy1

---
+++ src/main/java/me/seo/studyjpa/StudyjpaApplication.java
@@ -0,0 +1,13 @@
+package me.seo.studyjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyjpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyjpaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/seo/studyjpa/StudyjpaApplicationTests.java
@@ -0,0 +1,13 @@
+package me.seo.studyjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyjpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
